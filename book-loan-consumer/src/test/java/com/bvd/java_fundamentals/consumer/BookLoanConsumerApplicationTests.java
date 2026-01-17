fd4d9c7b9a60850ff973636bfae544829af7ec10
Birsan Ionut <birsan.ionut210392@gmail.com>
2026-01-18 00:47:24 +0200

    created  book loan consummer

---
+++ book-loan-consumer/src/test/java/com/bvd/java_fundamentals/consumer/BookLoanConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvd.java_fundamentals.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoanConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
